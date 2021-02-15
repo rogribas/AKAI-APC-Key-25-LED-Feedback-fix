58401469275364e9fa0241b8369ec1b9faa8855c
rogribas <rribasn@gmail.com>
2021-02-15 22:58:52 +0100

    Add color values list to script

--- apc_led_feedback.py
+++ apc_led_feedback.py
@@ -8,6 +8,16 @@ from mido import Message
 portname = 'IAC Driver Bus 1' # The virtual midi input port
 portname_send = 'APC Key 25'
 
+# COLOR LED VALUES (values to be sent as "velocity")
+# 0=off,
+# 1=green,
+# 2=green blink,
+# 3=red,
+# 4=red blink,
+# 5=yellow,
+# 6=yellow blink,
+# 7-127=green
+
 while True:
     if portname_send not in mido.get_output_names() \
             or portname not in mido.get_input_names():
@@ -25,6 +35,7 @@ while True:
                     for message in port:
                         print('Received {}'.format(message))
                         if message.type == 'note_off' and message.velocity == 64:
+                            # velocity = 3 -> RED Color
                             on = Message('note_on', note=message.note, velocity=3)
                             port_send.send(on)
                             sys.stdout.flush()
